cf6890bb4fdb720c8f0555190afa4d227e7c68d7
David L <77948280+TechFun4@users.noreply.github.com>
2023-01-03 14:47:37 -0800

    Update main.py
    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pypresence import Presence 
+import random
 import time
 
 start = int(time.time())
@@ -6,12 +7,15 @@ client_id = "1059963819700531240"
 RPC = Presence(client_id)
 RPC.connect()
 
+amount = 0.0000000
+
 while True: #infinite loop
     RPC.update(
-        large_image = "Mining Bitcoins", #name of your asset
-        large_text = "",
+        large_image = "bitcoin", #name of your asset
+        large_text = "Mining Bitcoins",
         details = "Mining...",
-        state = "",
+        state = f"Status: {amount:.7f} Mined",
         start = start,
-        buttons = [{"label": "Bitcoin Price", "url": "https://coinmarketcap.com/currencies/bitcoin/"})
+        buttons = [{"label": "Coinbase", "url": "https://www.coinbase.com/"}, {"label": "Bitcoin Price", "url": "https://coinmarketcap.com/currencies/bitcoin/"}])
     time.sleep(15) #can be as low as 15, depends on how often you want to update
+    amount += random.uniform(0.0000001, 0.0000010)
