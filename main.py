4442814d390033eb42ef5c832b8d8ce14fbd466d
David L <77948280+TechFun4@users.noreply.github.com>
2023-01-03 15:00:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ amount = 0.0000000
 
 while True: #infinite loop
     RPC.update(
-        large_image = "bitcoin", #name of your asset
+        large_image = "bitcoin",
         large_text = "Mining Bitcoins",
         details = "Mining...",
         state = f"Status: {amount:.7f} BTC Mined",
         start = start,
         buttons = [{"label": "Coinbase", "url": "https://www.coinbase.com/"}, {"label": "Bitcoin Price", "url": "https://coinmarketcap.com/currencies/bitcoin/"}])
-    time.sleep(15) #can be as low as 15, depends on how often you want to update
+    time.sleep(15)
     amount += random.uniform(0.0000001, 0.0000010)
